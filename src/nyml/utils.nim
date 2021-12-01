c55e3eabd5fb9474575dac78f52b366de7840c8c
George Lemon <georgelemon@protonmail.com>
2021-12-01 20:21:11 +0200

    Utilities
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/nyml/utils.nim
+++ src/nyml/utils.nim
@@ -10,3 +10,10 @@ proc cutLast*(text: string, cuts=2): string =
     # Remove chars from the end of the string
     try: text[0 .. ^cuts]
     except RangeDefect: text
+
+proc parseBoolValue*(v: string): bool =
+    # Try parse given string value into a bool
+    case v:
+    of "TRUE", "True", "true", "YES", "Yes", "yes": result = true
+    of "FALSE", "False", "false", "NO", "No", "no": result = false
+    else: raise newException(ValueError, "cannot interpret as a bool: " & v)
