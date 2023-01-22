a4106cb2e8b666a884c3e76a34d0948237a2da7f
George Lemon <georgelemon@protonmail.com>
2023-01-22 10:54:39 +0200

    fix
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/nyml/parser.nim
+++ src/nyml/parser.nim
@@ -267,8 +267,6 @@ proc parseArray(p: var Parser, node: Node, this: TokenTuple) =
         node.items.add objectNode
         # while p.curr.kind == TK_IDENTIFIER and p.curr.col == subNode.meta.col:
           # objectNode.value.add p.parse()
-    else:
-      echo p.curr
 proc parseInlineArray(p: var Parser, this: TokenTuple): Node =
   result = p.newNode Array
   walk p
